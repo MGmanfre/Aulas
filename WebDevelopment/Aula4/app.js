b7174559c6783b96db5aee3c726d470abccde165
MGmanfre <miguelmanfre0603@gmail.com>
2025-04-03 22:42:53 -0300

    aula1 javascript

---
+++ WebDevelopment/Aula4/app.js
@@ -0,0 +1,31 @@
+// window.alert("Hello World!");
+// let seunome = prompt("Qual é o seu nome?", "Insira aqui seu nome");
+// window.alert(seunome);
+// let resultado = confirm("Você deseja seguir?")
+// let são variaveis
+// let nome = "MGManfre";
+// let numero = 6;
+// let infinito = 1 / 0;
+// let nah = "fnweonew" / 8
+// let nulo = null
+// let ndefinido
+// let igual = 11 === 11
+// const são constantes
+// const apiKey = "fwonownffisfsjpowe";
+
+// console.log(`Olá,${seunome}, tudo bem?`)
+// console.log(typeof nome);
+// console.log(numero);
+// console.log(infinito);
+// console.log(nah);
+// console.log(nulo);
+// console.log(ndefinido);
+// console.log(igual);
+
+let numero1 = parseInt(prompt("Digite um número"));
+let numero2 = parseInt(prompt("Digite outro número"));
+let resultado = numero1 + numero2
+
+alert(String(resultado));
+
+
