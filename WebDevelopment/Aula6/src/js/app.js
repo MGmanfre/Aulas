aff311323a19b321d517c9e0c936cb0ac512f671
Miguel <miguelmanfre0603@gmail.com>
2025-04-10 22:42:56 -0300

    Aula6

---
+++ WebDevelopment/Aula6/src/js/app.js
@@ -0,0 +1,68 @@
+// let nome = "Miguel"
+
+// if (nome === "Miguel" || nome === null) {
+//     console.log(`É você ${nome}`)
+// }
+// else if (nome === `Fulano`) {
+//     console.log(`é o Fulano, não vc ${nome}`)
+// }
+// else {
+//     console.log(`Não é você`)
+// }
+
+// // nome === "Miguel" ? console.log('é você') : console.log('Sim')
+
+// if (nome === "Miguel" && nome === null) {
+//     console.log(`É você ${nome}`)
+// }
+// else if (nome === `Fulano`) {
+//     console.log(`é o Fulano, não vc ${nome}`)
+// }
+// else {
+//     console.log(`Não é você`)
+// }
+
+// Desafio 1
+// let user = String(prompt("Registre seu nome de usuário: "))
+// let password = prompt("Digite uma senha")
+
+// validateU = String(prompt("Digite o login: "))
+// validateP = prompt("Digite sua senha: ")
+
+// if (validateU === user && validateP === password) {
+//     alert("Login efetuado com sucesso!")
+// }
+// else {
+//     alert("Usuário ou senha incorreto!")
+// }
+
+// Desafio 2
+
+// let user = String(prompt("Registre seu nome de usuário: "))
+// let password = prompt("Digite uma senha")
+
+// validateU = String(prompt("Digite o login: "))
+// validateP = prompt("Digite sua senha: ")
+
+// if (validateU !== user || validateP !== password) {
+//     alert("Usuário ou senha incorreto!")
+// }
+// else {
+//     alert("Login efetuado com sucesso!")
+// }
+
+// Desafio 3
+
+// Desafio 4
+
+// Desafio 5
+
+let user = String(prompt("Registre seu nome de usuário: "))
+let password = prompt("Digite uma senha")
+
+validateU = String(prompt("Digite o login: "))
+validateP = prompt("Digite sua senha: ")
+
+if (validateU === user || validateP === password) {
+
+}
